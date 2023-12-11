3cd2d7dae2c67abb80e2b531e7c129a5a1e0bf6c
Rishi Raj <rishiraj007bgp@gmail.com>
2023-12-11 21:05:33 +0530

    focus code input on btn click

---
+++ script.js
@@ -0,0 +1,6 @@
+const joinMeetBtn1 = document.querySelector('.join-meet-btn1')
+const joinMeetBtn2 = document.querySelector('.join-meet-btn2')
+const codeInput = document.querySelector('.input-txt')
+
+joinMeetBtn1.addEventListener('click', () => codeInput.focus())
+joinMeetBtn2.addEventListener('click', () => codeInput.focus())
